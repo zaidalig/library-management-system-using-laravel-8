b689af2263fec5b367af725fbf26479365d8830c
zaidaliz <me.mzaidali@gmail.com>
2024-03-21 06:48:48 +0500

    basic setup added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <span>© Copyright {{ now()->format("Y") }} <a href="https://www.yahoobaba.net">YahooBaba 😎</a></span>
+                    <span>© Copyright {{ now()->format("Y") }} <a href="https://github.com/zaidalig">Muhammad Zaid Ali 😎</a></span>
                 </div>
             </div>
         </div>
